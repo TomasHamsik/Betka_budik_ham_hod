f2d5481328035bc6c3119fb7e67b7e7b698adad3
TomasHamsik <85436894+TomasHamsik@users.noreply.github.com>
2021-06-06 20:01:02 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,33 +2,41 @@ function HOD_Beh () {
     basic.pause(1000)
     if (HOD_PRED_milis + 1000 < control.millis()) {
         HOD_TED_milis = control.millis()
-        vypis2serial("" + "___\ndist_milis: " + (HOD_TED_milis - HOD_PRED_milis), 1)
         HOD_s_b = (HOD_sec + Math.trunc((HOD_TED_milis - HOD_PRED_milis) / 1000)) % 60
         HOD_prenos = Math.trunc((HOD_sec + Math.trunc((HOD_TED_milis - HOD_PRED_milis) / 1000)) / 60)
-        vypis2serial("HOD_sec: " + HOD_sec + " pren2min: " + HOD_prenos, 1)
         HOD_m_b = (HOD_prenos + HOD_min) % 60
         HOD_prenos = Math.trunc((HOD_prenos + HOD_min) / 60)
-        vypis2serial("HOD_min: " + HOD_min + " pren2hod: " + HOD_prenos, 1)
         HOD_hod = (HOD_prenos + HOD_hod) % 24
         HOD_sec = HOD_s_b
         HOD_min = HOD_m_b
         HOD_mili_b = (HOD_TED_milis - HOD_PRED_milis) % 1000
         HOD_PRED_milis = HOD_TED_milis - HOD_mili_b
-        vypis2serial("Cas: " + ("" + HOD_hod + ":" + HOD_min + ":" + HOD_sec + "\n____"), 1)
     }
+}
+function HOD_on_strat () {
+    HOD_PRED_milis = control.millis()
 }
 // a = array
 function pridej_budik () {
-    a_budik_y.push([6, 0, 24, 0])
-    a_budik_y.push([7, 30, 24, 1])
-    a_budik_y.push([13, 15, 3, 2])
-    a_budik_y.push([7, 0, 8, 3])
+    a_budik_y.push([6, 0, 24, 0, 1])
+    a_budik_y.push([7, 30, 24, 1, 1])
+    a_budik_y.push([13, 15, 3, 2, 1])
+    a_budik_y.push([7, 0, 8, 3, 1])
 }
 function mam_zvonit () {
     for (let a_akt_budik of a_budik_y) {
-        if (kitronik_RTC.readHours() >= a_akt_budik[0]) {
-            if (kitronik_RTC.readMinutes() >= a_akt_budik[1]) {
-                basic.showString("" + (a_nazvy_upozorneni[a_akt_budik[3]]))
+        if (1 == a_akt_budik[4]) {
+            if (HOD_hod >= a_akt_budik[0]) {
+                if (HOD_min >= a_akt_budik[1]) {
+                    a_akt_budik[0] = a_akt_budik[0] + a_akt_budik[2]
+                    if (a_akt_budik[0] >= 24) {
+                        a_akt_budik[0] = a_akt_budik[0] - 24
+                        a_akt_budik[4] = 0
+                    }
+                    while (!(input.buttonIsPressed(Button.AB))) {
+                        basic.showString("" + (a_nazvy_upozorneni[a_akt_budik[3]]))
+                    }
+                }
             }
         }
     }
@@ -38,17 +46,6 @@ function vypis2serial (hlaska: string, uroven: number) {
         serial.writeLine("[" + convertToText(uroven) + "] " + "[" + hlaska + "]")
     }
 }
-/**
- * budík je pole
- * 
- * jeho hodnoty (parametry) jsou:
- * 
- * [cas nastavajiciho zazvoneni,
- * 
- * interval (opakovani) (h),
- * 
- * symbol]
- */
 let HOD_mili_b = 0
 let HOD_m_b = 0
 let HOD_prenos = 0
@@ -63,27 +60,37 @@ let vypis_urovne = 0
 let HOD_PRED_milis = 0
 let HOD_h_b = 0
 HOD_PRED_milis = control.millis()
-vypis2serial("pred_milis: " + HOD_PRED_milis, 1)
-vypis_urovne = 2
-vypis2serial("start", 1)
-vypis2serial("10/3: " + convertToText(10 / 3), 1)
-vypis2serial("rem 10/3: " + convertToText(10 % 3), 1)
+vypis_urovne = 4
 a_budik_y = []
 pridej_budik()
 a_nazvy_upozorneni = ["vstavej", "cviceni", "tablety", "zalit kytky"]
-HOD_hod = 23
-HOD_min = 59
-HOD_sec = 55
+HOD_on_strat()
+HOD_hod = 13
+HOD_min = 14
+HOD_sec = 25
 vypis2serial("pred nastav hodiny", 1)
 vypis2serial("po nastav hodiny", 1)
 vypis2serial("Cas: " + ("" + HOD_hod + ":" + HOD_min + ":" + HOD_sec), 1)
 vypis2serial("Datum: " + kitronik_RTC.readDate(), 1)
-basic.forever(function () {
-    mam_zvonit()
-})
+/**
+ * budík je pole
+ * 
+ * jeho hodnoty (parametry) jsou:
+ * 
+ * [cas nastavajiciho zazvoneni,
+ * 
+ * interval (opakovani) (h),
+ * 
+ * symbol]
+ */
 basic.forever(function () {
 	
 })
-control.inBackground(function () {
+basic.forever(function () {
     HOD_Beh()
+    vypis2serial("" + convertToText(HOD_hod) + " : " + convertToText(HOD_min) + " : " + convertToText(HOD_sec), 4)
+})
+basic.forever(function () {
+    basic.pause(500)
+    mam_zvonit()
 })
